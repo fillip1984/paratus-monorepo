5bb4b15ed9323398522102231aa39d90eea1b638
fillip1984 <fillip1984@gmail.com>
2025-08-24 11:26:13 -0400

    added ability to sort sections and tasks

--- apps/paratus-web/src/app/_components/collection/CollectionListView.tsx
+++ apps/paratus-web/src/app/_components/collection/CollectionListView.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import type { CollectionDetailType } from "@paratus/api";
+import { useEffect } from "react";
+import { useDragAndDrop } from "@formkit/drag-and-drop/react";
 
+import type { CollectionDetailType, SectionDetailType } from "@paratus/api";
+
+import { api } from "~/trpc/react";
 import SectionCard from "./section/SectionCard";
 
 export default function CollectionListView({
@@ -9,11 +13,46 @@ export default function CollectionListView({
 }: {
   collection: CollectionDetailType;
 }) {
+  // DnD
+  const [parentRef, draggableSections, setValues] = useDragAndDrop<
+    HTMLDivElement,
+    SectionDetailType
+  >([], {
+    dragHandle: ".drag-handle",
+    group: "collection",
+    disabled: collection.name === "Today" || collection.name === "Upcoming",
+    onDragend: (data) => {
+      reorderSections(
+        data.values.map((section, index) => ({
+          id: (section as SectionDetailType).id,
+          position: index,
+        })),
+      );
+    },
+  });
+  useEffect(() => {
+    setValues(collection.sections);
+  }, [collection, setValues]);
+
+  const trpc = api.useUtils();
+  const { mutate: reorderSections } = api.section.reorder.useMutation({
+    onSuccess: async () => {
+      await trpc.collection.readAll.invalidate();
+      await trpc.collection.readOne.invalidate({
+        id: collection.id,
+      });
+      await trpc.task.today.invalidate();
+    },
+  });
+
   return (
     <div className="flex flex-1 flex-col overflow-hidden">
       <div className="snap-y snap-mandatory overflow-y-auto p-2 pb-12">
-        <div className="flex w-full max-w-[800px] flex-col gap-2 lg:mx-auto">
-          {collection.sections.map((section) => (
+        <div
+          ref={parentRef}
+          className="flex w-full max-w-[800px] flex-col gap-2 lg:mx-auto"
+        >
+          {draggableSections.map((section) => (
             <SectionCard
               key={section.id}
               collection={collection}
