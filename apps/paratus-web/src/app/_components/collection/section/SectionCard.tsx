8a99daf706d6e7205602288cf0fc87432c487b31
fillip1984 <fillip1984@gmail.com>
2025-09-01 09:06:57 -0400

    fixed bug where adding from today or upcoming failed due to missing sectionId

--- apps/paratus-web/src/app/_components/collection/section/SectionCard.tsx
+++ apps/paratus-web/src/app/_components/collection/section/SectionCard.tsx
@@ -33,19 +33,27 @@ export default function SectionCard({
   // determine default due date
   const [defaultDueDate, setDefaultDueDate] = useState<Date | null>(null);
   const [defaultSectionId, setDefaultSectionId] = useState<string>(section.id);
+  const { data: inboxId } = api.collection.inboxId.useQuery();
+  const { data: inbox } = api.collection.readOne.useQuery(
+    { id: inboxId ?? "" },
+    { enabled: !!inboxId },
+  );
+
   const path = usePathname();
   useEffect(() => {
+    if (!inboxId) return;
+    const uncategorized = inbox?.sections.find(
+      (s) => s.name === "Uncategorized",
+    );
+    if (!uncategorized) return;
     if (path === "/today") {
       setDefaultDueDate(startOfDay(new Date()));
-      setDefaultSectionId("inbox");
+      setDefaultSectionId(uncategorized.id);
     } else if (path === "/upcoming") {
-      console.log({ collectionName: collection.name, sectionId: section.id });
-      const dd = new Date(Number(section.id));
-      console.log(dd);
-      setDefaultDueDate(dd);
-      setDefaultSectionId("inbox");
+      setDefaultDueDate(new Date(Number(section.id)));
+      setDefaultSectionId(uncategorized.id);
     }
-  }, [collection.name, path, section.id]);
+  }, [collection.name, path, section.id, inboxId, inbox]);
 
   const [isAddTaskOpen, setIsAddTaskOpen] = useState(false);
   const [isSectionCollapsed, setIsSectionCollapsed] = useState(false);
